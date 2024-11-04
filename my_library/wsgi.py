a702f62ee2c585ce57b46c6c830f7ee5f55ae1cd
Alejandro Newport <anewportd@hotmail.com>
2024-11-04 15:37:10 +0000

    /var/www/k23093489_pythonanywhere_com_wsgi.py

--- my_library/wsgi.py
+++ my_library/wsgi.py
@@ -4,13 +4,13 @@ WSGI config for my_library project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/stable/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Set the default Django settings module for the 'my_library' project
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_library.settings')
 
 application = get_wsgi_application()
